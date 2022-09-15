a3f2d607e82b3e7ad4d1e6640734e977f76ef3ab
cutano <rinne@rinne.com>
2022-09-15 13:09:20 +0800

    Init repo.

---
+++ AkariRenderer/Src/AkariRenderer.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+
+#include <glfw/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <glfw/glfw3native.h>
+
+int main()
+{
+    const int width = 800, height = 600;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    auto window = glfwCreateWindow(width, height, "glfw - DX12", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Creat window error" << std::endl;
+        return -1;
+    }
+
+    auto process_keystrokes_input = [](GLFWwindow* window)
+    {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, 1);
+    };
+
+    auto hMainWnd = glfwGetWin32Window(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        process_keystrokes_input(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
---
+++ AkariRenderer/Src/pch.h
@@ -0,0 +1,63 @@
+﻿# pragma once
+
+#include <winsdkver.h>
+#define _WIN32_WINNT 0x0A00
+#include <sdkddkver.h>
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+// DirectX apps don't need GDI
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+
+// Include <mcx.h> if you need this
+#define NOMCX
+
+// Include <winsvc.h> if you need this
+#define NOSERVICE
+
+// WinHelp is deprecated
+#define NOHELP
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
+#include <wrl/client.h>
+#include <wrl/event.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include "d3dx12.h"
+#ifdef _DEBUG
+    #include <dxgidebug.h>
+#endif
+
+#define D3D12_GPU_VIRTUAL_ADDRESS_NULL      ((D3D12_GPU_VIRTUAL_ADDRESS)0)
+#define D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN   ((D3D12_GPU_VIRTUAL_ADDRESS)-1)
+#define MY_IID_PPV_ARGS                     IID_PPV_ARGS
+
+
+#include <cstdint>
+#include <cstdio>
+#include <cstdarg>
+#include <cwctype>
+#include <exception>
+#include <memory>
+#include <vector>
+#include <string>
+#include <array>
+#include <unordered_map>
+#include <algorithm>
+#include <random>
+#include <fstream>
+#include <functional>
+#include <ppltasks.h>
+
+
+extern "C" {
+inline __declspec(dllexport) extern constexpr UINT D3D12SDKVersion = 606;
+inline __declspec(dllexport) extern const char8_t* D3D12SDKPath = u8".\\D3D12\\";
+}
