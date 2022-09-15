3e16bc30d2631d45a0d19f1152f7bdce1ab544d6
cutano <rinne@rinne.com>
2022-09-15 14:35:08 +0800

    Fix .gitignore.

--- AkariRenderer/Src/pch.h
+++ AkariRenderer/Src/pch.h
@@ -28,6 +28,11 @@
 #include <wrl/client.h>
 #include <wrl/event.h>
 
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include "d3dx12.h"
