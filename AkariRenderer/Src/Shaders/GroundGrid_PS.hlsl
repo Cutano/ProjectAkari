b450bab479a7c8667de459472bf3384375535e42
Cutano <rinne@rinne.top>
2022-09-27 15:18:57 +0800

    Set shader level to 5.1.

--- AkariRenderer/Src/Shaders/GroundGrid_PS.hlsl
+++ AkariRenderer/Src/Shaders/GroundGrid_PS.hlsl
@@ -12,5 +12,5 @@ struct PS_INPUT
 float4 main(PS_INPUT input) : SV_TARGET
 {
 	float3 V = normalize(ViewPosWS.xyz - input.posWS);
-	return float4(1.0f, 1.0f, 1.0f, dot(V, float3(0, 1, 0)));
+	return float4(0.8f, 0.8f, 0.8f, dot(V, float3(0, 1, 0)) * 0.8f);
 }
