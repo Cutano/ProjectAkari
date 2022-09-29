86111f4a3386b289d4d3e8818e1cb812ddfed75f
Cutano <rinne@rinne.top>
2022-09-29 16:19:20 +0800

    Adjust grid style when y is negative.

--- AkariRenderer/Src/Shaders/GroundGrid_PS.hlsl
+++ AkariRenderer/Src/Shaders/GroundGrid_PS.hlsl
@@ -12,6 +12,8 @@ struct PS_INPUT
 float4 main(PS_INPUT input) : SV_TARGET
 {
 	// float3 clampedYViewPos = float3(ViewPosWS.x, clamp(ViewPosWS.y, -20.0f, 20.0f), ViewPosWS.z);
-	float3 V = normalize(ViewPosWS.xyz - input.posWS);
-	return float4(0.8f, 0.8f, 0.8f, clamp(pow(dot(V, float3(0, 1, 0)), 1.4f) * pow(1.1f, -ViewPosWS.y), -0.7f, 0.7f));
+	float3 ViewWS = ViewPosWS;
+	ViewWS.y = abs(ViewPosWS.y);
+	float3 V = normalize(ViewWS.xyz - input.posWS);
+	return float4(0.8f, 0.8f, 0.8f, clamp(pow(dot(V, float3(0, 1, 0)), 1.4f) * pow(1.1f, -ViewWS.y), 0.0f, 0.8f));
 }
