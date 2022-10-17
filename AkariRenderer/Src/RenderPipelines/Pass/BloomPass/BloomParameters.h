7fb929fb426f2f9259569e89a58353a5da80d693
Cutano <rinne@rinne.top>
2022-10-17 16:40:10 +0800

    Adjust bloom default clamping value.

--- AkariRenderer/Src/RenderPipelines/Pass/BloomPass/BloomParameters.h
+++ AkariRenderer/Src/RenderPipelines/Pass/BloomPass/BloomParameters.h
@@ -6,7 +6,7 @@ namespace Akari
     {
         float Threshold       = 1.0f;
         float Intensity       = 1.0f;
-        float Clamp           = 65536.0f;
+        float Clamp           = 8.0f;
         float SoftKnee        = 0.5f;
         float Diffusion       = 7.0f;
         float AnamorphicRatio = 0.0f;
