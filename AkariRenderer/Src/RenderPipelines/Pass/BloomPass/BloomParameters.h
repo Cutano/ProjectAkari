2409b9780706297bf6035b7171eebe6ca9cf45dd
Cutano <rinne@rinne.top>
2022-10-20 19:32:06 +0800

    Adjust default bloom settings.

--- AkariRenderer/Src/RenderPipelines/Pass/BloomPass/BloomParameters.h
+++ AkariRenderer/Src/RenderPipelines/Pass/BloomPass/BloomParameters.h
@@ -5,12 +5,12 @@ namespace Akari
     struct BloomParameters
     {
         float Threshold       = 1.0f;
-        float Intensity       = 1.0f;
+        float Intensity       = 0.2f;
         float Clamp           = 65535.0f;
         float SoftKnee        = 0.0f;
-        float Diffusion       = 10.0f;
+        float Diffusion       = 8.0f;
         float AnamorphicRatio = 0.0f;
-        bool  LowQuality      = true;
+        bool  LowQuality      = false;
     };
 
     extern BloomParameters g_BloomParameters;
